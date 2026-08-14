7ef13c2337e7f3ac9590344c3f794e3054d86278
Rexix01 <rexix01@proton.me>
2026-08-14 19:05:20 +0200

    tesla: Switch to 6GB dalvik heap

--- device.mk
+++ device.mk
@@ -7,13 +7,7 @@
 DEVICE_PATH := device/motorola/tesla
 
 # Dalvik
-PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapstartsize=24m \
-    dalvik.vm.heapgrowthlimit=256m \
-    dalvik.vm.heapsize=512m \
-    dalvik.vm.heaptargetutilization=0.46 \
-    dalvik.vm.heapminfree=8m \
-    dalvik.vm.heapmaxfree=48m
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
 # Init
 PRODUCT_PACKAGES += \
